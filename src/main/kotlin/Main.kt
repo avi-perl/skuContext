cb88fb56fcd07d0d8f16294bff232d2ec7a572cb
avi-perl <avi@avrohomperl.com>
2023-09-26 00:27:53 -0400

    Use Clikt lib

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,59 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
+import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStream
 import java.time.LocalDateTime
 import kotlin.system.exitProcess
 
-data class Entry(val value: String, val timestamp: LocalDateTime) {
+data class Entry(val value: String, val timestamp: LocalDateTime, val id: String?) {
     companion object {
-        fun create(value: String) = Entry(value, LocalDateTime.now())
+        fun create(value: String, id: String?) = Entry(value, LocalDateTime.now(), id)
     }
 }
 
 fun OutputStream.writeEntry(entry: Entry) {
     bufferedWriter().let {
-        it.appendLine("${entry.timestamp.toString()}, ${entry.value}")
+        it.appendLine("${entry.timestamp.toString()}, ${entry.value}, ${entry.id ?: ""}")
         it.flush()
     }
 }
 
+class SKUContext() : CliktCommand() {
+    override fun run() = Unit
+}
 
-fun main(args: Array<String>) {
-    println("Begin entering SKUs:")
+class LogSKU : CliktCommand(help = "Log entries to a file with timestamps") {
+    private val file by argument(help = "The CSV file that your entries should be saved in").file(
+        canBeDir = false,
+        mustBeWritable = true
+    )
+    private val scannerId by option(help = "Identification for independent tracking")
 
-    FileOutputStream("output.csv", true).let { file ->
-        while (true) {
-            readlnOrNull()?.let { input ->
-                if (input.equals("exit", ignoreCase = true)) {
-                    exitProcess(0)
+    override fun run() {
+        File(file.name).let {
+            if (!it.exists()) {
+                it.createNewFile()
+                it.appendText("timestamp, id, value\n")
+            }
+        }
+
+        FileOutputStream(file.name, true).let { file ->
+            while (true) {
+                readlnOrNull()?.let { input ->
+                    if (input.equals("exit", ignoreCase = true)) {
+                        exitProcess(0)
+                    }
+
+                    file.writeEntry(Entry.create(input, scannerId))
                 }
-
-                file.writeEntry(Entry.create(input))
-                println("Text logged to '$input'.")
             }
         }
     }
 }
+
+
+fun main(args: Array<String>) = SKUContext().subcommands(LogSKU()).main(args)
