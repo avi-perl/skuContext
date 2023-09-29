39e6f596a900c8b80b0d91c1d347204d5a9049ac
avi-perl <avi@avrohomperl.com>
2023-09-29 00:20:31 -0400

    WIP file load

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,59 +1,28 @@
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.core.*
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.file
+import utils.LogSKU
+import utils.CategorizeFiles
 import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
-import java.time.LocalDateTime
-import kotlin.system.exitProcess
 
-data class Entry(val value: String, val timestamp: LocalDateTime, val id: String?) {
-    companion object {
-        fun create(value: String, id: String?) = Entry(value, LocalDateTime.now(), id)
-    }
-}
-
-fun OutputStream.writeEntry(entry: Entry) {
-    bufferedWriter().let {
-        it.appendLine("${entry.timestamp.toString()}, ${entry.value}, ${entry.id ?: ""}")
-        it.flush()
-    }
-}
+val logSkuInstance = LogSKU()
 
 class SKUContext() : CliktCommand() {
-    override fun run() = Unit
-}
-
-class LogSKU : CliktCommand(help = "Log entries to a file with timestamps") {
-    private val file by argument(help = "The CSV file that your entries should be saved in").file(
-        canBeDir = false,
-        mustBeWritable = true
+    private val logFile by argument(help = "The CSV file that your entries should be saved in").file(
+        canBeDir = false
     )
-    private val scannerId by option(help = "Identification for independent tracking")
+    private val config by findOrSetObject { mutableMapOf<String, String>() }
 
     override fun run() {
-        File(file.name).let {
-            if (!it.exists()) {
-                it.createNewFile()
-                it.appendText("timestamp, id, value\n")
-            }
+        // For all commands except the logging creation, the file must exist.
+        // TODO: Allow for when a subcommand --help is called
+        if (currentContext.invokedSubcommand != logSkuInstance && !File(logFile.name).exists()) {
+            throw CliktError("File does not exist: ${logFile.name}")
         }
 
-        FileOutputStream(file.name, true).let { file ->
-            while (true) {
-                readlnOrNull()?.let { input ->
-                    if (input.equals("exit", ignoreCase = true)) {
-                        exitProcess(0)
-                    }
-
-                    file.writeEntry(Entry.create(input, scannerId))
-                }
-            }
-        }
+        config["logFile"] = logFile.name
     }
 }
 
 
-fun main(args: Array<String>) = SKUContext().subcommands(LogSKU()).main(args)
+fun main(args: Array<String>) = SKUContext().subcommands(logSkuInstance, CategorizeFiles()).main(args)
---
+++ src/main/kotlin/utils/CategorizeFiles.kt
@@ -0,0 +1,43 @@
+package utils
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.findOrSetObject
+import com.github.ajalt.clikt.core.requireObject
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.boolean
+import com.github.ajalt.clikt.parameters.types.file
+import java.io.File
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.attribute.BasicFileAttributes
+
+class CategorizeFiles : CliktCommand(help = "Categorize files based on the entries in the log file") {
+    private val config by requireObject<Map<String, String>>()
+    private val filesDir by argument(help = "Path to a dir of files to categorize").file(
+        mustExist = true,
+        canBeFile = false,
+        mustBeReadable = true,
+    )
+    private val recursive by option(help = "Explore the directory recursively").boolean().default(false)
+
+    private fun listFiles(dir: File, allowRecursive: Boolean = true, handleFile: (File) -> Unit) {
+        dir.listFiles()?.forEach { file ->
+            if (file.isFile) {
+                handleFile(file)
+            } else if (allowRecursive && file.isDirectory) {
+                listFiles(file, true, handleFile)
+            }
+        }
+    }
+
+    override fun run() {
+        println(Entry.loadFile(File(config["logFile"] ?: "")))
+        listFiles(filesDir, recursive) {
+            Files.readAttributes(FileSystems.getDefault().getPath(it.path), BasicFileAttributes::class.java).let { metadata ->
+                println(metadata.creationTime())
+            }
+        }
+    }
+}
